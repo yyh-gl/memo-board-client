d43a79d139fcec699b1734ce3500613edfe92c90
yy_h <yhonda@mikilab.doshisha.ac.jp>
2018-02-21 20:27:49 +0900

    First Commit

---
+++ memo-board/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memo-board
+//
+//  Created by yupouri on 2018/02/21.
+//  Copyright © 2018年 Y_h. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
